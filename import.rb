bac5bca36d113e77ef8146b94d9d167d9b920496
margonaut <margaret.morgane@gmail.com>
2015-08-27 00:43:36 -0400

    Filtered data set down to words with phonetic transcriptions

--- import.rb
+++ import.rb
@@ -10,3 +10,9 @@ def db_connection
     connection.close
   end
 end
+
+db_connection do |conn|
+  CSV.foreach('words.csv', headers: true, header_converters: :symbol) do |row|
+    conn.exec("INSERT INTO words (english, phonetic) VALUES ($1, $2)", [row[:english], row[:phonetic]])
+  end
+end
--- schema.sql
+++ schema.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS words;
 CREATE TABLE words (
   id SERIAL PRIMARY KEY,
   english varchar(100) NOT NULL,
-  phonetic varchar(100) NOT NULL
+  phonetic varchar(100) NOT NULL,
+  reading_level int,
+  syllables int
 );
